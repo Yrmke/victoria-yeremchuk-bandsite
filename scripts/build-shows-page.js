24862617c8de70ee90ac85a190330becd47b8c99
yerema <maibleyeremchuk@gmail.com>
2023-09-04 13:35:48 -0400

    ignored the bio.css.map file

--- scripts/build-shows-page.js
+++ scripts/build-shows-page.js
@@ -1,126 +1,99 @@
 
-const shows = [
-    {
-        date: "Mon Sept 06 2021",
-        venue: "Ronald Lane",
-        location: "San Francisko, CA"
-    },
-    {
-        date: "Tue Sept 21 2021",
-        venue: "Pier 3 East",
-        location: "San Francisko, CA"
-    },
-    {
-        date: "Fri Oct 15 2021",
-        venue: "View Lounge",
-        location: "San Francisko, CA"
-    },
-    {
-        date: "Sat Nov 06 2021",
-        venue: "Hyatt Agency",
-        location: "San Francisko, CA"
-    },
-    {
-        date: "Fri Nov 26 2021",
-        venue: "Moscow Center",
-        location: "San Francisko, CA"
-    },
-    {
-        date: "Wed Dec 15 2021",
-        venue: "Press Club",
-        location: "San Francisko, CA"
-    }
-]
+const apiKey = "8d7811c5-bb68-4bf0-8d6a-efe34ee81cfa";
+const usersURL = `https://project-1-api.herokuapp.com/showdates?api_key=${apiKey}`;
 
-// h2 title "SHOWS"
-const showSection = document.querySelector(".shows");
-const showTitle = document.createElement("h2");
-    showTitle.classList.add("shows-title");
-    showTitle.innerText = "Shows";
-    showSection.appendChild(showTitle);
 
-const displayShows = (tickets) =>{
+
+axios
+  .get(usersURL)
+  .then((response) => {
+    console.log(response.data);
+
     const showSection = document.querySelector(".shows");
+    const showTitle = document.createElement("h2");
+        showTitle.classList.add("shows-title");
+        showTitle.innerText = "Shows";
+        showSection.appendChild(showTitle);
 
-    // h2 title
-    // const showTitle = document.createElement("h2");
-    // showTitle.classList.add("shows-title");
-    // showTitle.innerText = "Shows";
-    // showSection.appendChild(showTitle);
+        // DATE VENUE LOCATION ROW
+        const showsRowContainer =  document.createElement("div");
+        showsRowContainer.classList.add("shows-row");
+        showSection.appendChild(showsRowContainer);
 
-    // list
-    const showList = document.createElement("ul");
-  showList.classList.add("shows-list");
-  showSection.appendChild(showList);
+        const showsRowDate = document.createElement("p")
+        showsRowDate.classList= ("shows-row__element")
+        showsRowDate.innerText = "DATE";
+        showsRowContainer.appendChild(showsRowDate);
 
-    // list items
-    const itemList = document.createElement("li");
-    itemList.classList.add("shows-list__item");
-    showList.appendChild(itemList);
+        const showsRowVenue = document.createElement("p")
+        showsRowVenue.classList= ("shows-row__element")
+        showsRowVenue.innerText = "VENUE";
+        showsRowContainer.appendChild(showsRowVenue);
 
-    // show info
-    // DATE
-    const titleItem = document.createElement("p");
-    titleItem.classList.add("shows-list__title");
-    titleItem.innerHTML = "DATE";
-    itemList.appendChild(titleItem);
+        const showsRowLocation = document.createElement("p")
+        showsRowLocation.classList= ("shows-row__element")
+        showsRowLocation.innerText = "LOCATION";
+        showsRowContainer.appendChild(showsRowLocation);
 
-    // DAY MON YEAR
-    const dateItem = document.createElement("p");
-    dateItem.classList.add("shows-list__infob");
-    dateItem.innerHTML = tickets.date;
-    itemList.appendChild(dateItem);
+        response.data.forEach((tickets) => {
 
-    // VENUE
-    const placeItem = document.createElement("p");
-    placeItem.classList.add("shows-list__title");
-    placeItem.innerHTML = "VENUE";
-    itemList.appendChild(placeItem);
+        // FORMATTED DATE
+        const date = new Date(tickets.date);
+        const formattedDate = date.toLocaleDateString('en-US', { weekday: 'short', year: 'numeric', month: 'short', day: 'numeric' });
+        
+        // list
+        const showList = document.createElement("ul");
+        showList.classList.add("shows-list");
+        showSection.appendChild(showList);
+        
+        // list items
+        const itemList = document.createElement("li");
+        itemList.classList.add("shows-list__item");
+        showList.appendChild(itemList);
     
-    // VENUE DETAILS
-    const venueItem = document.createElement("p");
-    venueItem.classList.add("shows-list__info");
-    venueItem.innerHTML = tickets.venue;
-    itemList.appendChild(venueItem);
-
-    // LOCATION
-    const areaItem = document.createElement("p");
-    areaItem.classList.add("shows-list__title");
-    areaItem.innerHTML = "LOCATION";
-    itemList.appendChild(areaItem);
+        // show info
+        // DATE
+        const titleItem = document.createElement("p");
+        titleItem.classList.add("shows-list__title");
+        titleItem.innerText = "DATE";
+        itemList.appendChild(titleItem);
     
-   
-    const locationItem = document.createElement("p");
-    locationItem.classList.add("shows-list__info");
-    locationItem.innerHTML = tickets.location;
-    itemList.appendChild(locationItem);
-
-    const button = document.createElement("button");
-    button.classList.add("show-list__button");
-    button.innerHTML = "BUY TICKETS";
-    itemList.appendChild(button);
-
-
-}
-
-//  FOR LOOP (ALL OF THE SHOWS)
-for (let i = 0; i< shows.length; i++) {
-    const tickets = shows[i];
-    displayShows(tickets);
+        // DAY MON YEAR
+        const dateItem = document.createElement("p");
+        dateItem.classList.add("shows-list__infob");
+        dateItem.innerText = formattedDate;
+        itemList.appendChild(dateItem);
     
-}
-
-/* <section class="shows">
-<h2 class="shows-title">Shows</h2>
-<ul class="shows-list">
-    <li class="shows-list__item">
-        <h3 class="shows-list__title">Date</h3>
-        <p class="shows-list__info">Mon Seot 06 2021</p>
-        <h3 class="shows-list__title">Venue</h3>
-        <p class="shows-list__info">Ronald Lane</p>
-        <h3 class="shows-list__title">Location</h3>
-        <p class="shows-list__info">San Francisko, CA</p>
-        <button>BUY TICKETS</button>
-    </li>
-</ul>
-</section> */
+        // VENUE
+        const placeItem = document.createElement("p");
+        placeItem.classList.add("shows-list__title");
+        placeItem.innerText = "VENUE";
+        itemList.appendChild(placeItem);
+        
+        // VENUE DETAILS
+        const venueItem = document.createElement("p");
+        venueItem.classList.add("shows-list__info");
+        venueItem.innerText = tickets.place;
+        itemList.appendChild(venueItem);
+    
+        // LOCATION
+        const areaItem = document.createElement("p");
+        areaItem.classList.add("shows-list__title");
+        areaItem.innerText = "LOCATION";
+        itemList.appendChild(areaItem);
+        
+        const locationItem = document.createElement("p");
+        locationItem.classList.add("shows-list__info");
+        locationItem.innerText = tickets.location;
+        itemList.appendChild(locationItem);
+    
+        const button = document.createElement("button");
+        button.classList.add("shows-list__button");
+        button.innerText = "BUY TICKETS";
+        itemList.appendChild(button);
+});
+})
+  .catch(function (error) {
+    console.log(error);
+  });
+  
--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,40 +1,24 @@
-const comments = [
-  {
-    name: "Connor Walton",
-    description:
-      "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.",
-    date: "02/17/2021",
-  },
-  {
-    name: "Emilie Beach",
-    description:
-      "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day.",
-    date: "01/09/2021",
-  },
-  {
-    name: "Miles Acosta",
-    description:
-      "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beau",
-    date: "12/20/2020",
-  },
-];
-//          <div class="previous-comment">
-//             <div class="previous-comment__pic"></div>
-//             <div class="previous-comment__content">
-//             <p class="previous-comment__name">Connor Walton</p>
-//             <p class="previous-comment__date">02/17/2021</p>
-//             <p class="previous-comment__description">
-//                 This is art. This is inexplicable magic expressed in the
-//                 purest way, everything that makes up this majestic work
-//                 deserves reverence. Let us appreciate this for what it
-//                 is and what it contains.
-//             </p>
-//             </div>
-//          </div>
+// API
 
-const displayComment = (comment) => {
-  const commentList = document.querySelector(".posted-comments");
+const apiKey = "8d7811c5-bb68-4bf0-8d6a-efe34ee81cfa";
+const usersURL = `https://project-1-api.herokuapp.com/comments?api_key=${apiKey}`;
+const commentList = document.querySelector(".posted-comments");
 
+  axios
+  .get(usersURL)
+  .then((response) => {
+    console.log(response.data);
+
+    response.data.reverse().forEach(displayComment);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+
+function displayComment(comment) {
+
+  // FORMATTED TIMESTAMP
+  const date = new Date(comment.timestamp);
   const commentEl = document.createElement("div");
   commentEl.classList.add("previous-comment");
   commentList.appendChild(commentEl);
@@ -44,7 +28,7 @@ const displayComment = (comment) => {
   commentEl.appendChild(profileImageElement);
 
   // comment content
-  
+
   // const includeContent = () =>{
   const commentElement = document.createElement("div");
   commentElement.classList.add("previous-comment__name");
@@ -53,78 +37,58 @@ const displayComment = (comment) => {
   // name
   const nameElement = document.createElement("p");
   nameElement.classList.add("previous-comment__name");
-  nameElement.innerHTML = comment.name;
+  nameElement.innerText = comment.name;
   commentEl.appendChild(nameElement);
-
+  console.log(nameElement);
   // date
   const dateElement = document.createElement("p");
   dateElement.classList.add("previous-comment__date");
-  dateElement.innerHTML = comment.date;
+  dateElement.innerText = date.getDate() + '/' +  date.getMonth() + '/' + date.getFullYear();
   commentEl.appendChild(dateElement);
 
   // description
   const descriptionElement = document.createElement("p");
   descriptionElement.classList.add("previous-comment__description");
-  descriptionElement.innerHTML = comment.description;
+  descriptionElement.innerText = comment.comment;
   commentEl.appendChild(descriptionElement);
-};
-
-for (let index = 0; index < comments.length; index++) {
-    const comment = comments[index];
-    displayComment(comment)
-    
-}
-
-//  NEW COMMENTS
-// const
-
-// // BUTTOON
-// const button = document.querySelector(".comments-form__button");
-// button.addEventListener("click", defaultComments);
-
-const form = document.querySelector('.comments-form')
-
-form.addEventListener('submit', handleSubmit)
-// button.addEventListener("reset", handleSubmit)
-// let timestamp = Date.now();
-// console.log(timestamp);
-
-// let d = newDate(timestamp);
-// console.log(d);
-let d = new Date();
-let date = d.getDate();
-let month = d.getMonth() + 1; // Since getMonth() returns month from 0-11 not 1-12
-let year = d.getFullYear();
-let dateStr = date + "/" + month + "/" + year;
-
-function handleSubmit (e) {
-    e.preventDefault()
-    const name = e.target.userName.value
-    const comment = e.target.userComment.value
-
-    console.log(name, comment)
-
-    const newComment = {
-        name: name,
-        description: comment,
-        date: dateStr,
-    }
-    displayComment(newComment)
 }
 
 
-/*
-Create htlm form
+const form = document.querySelector(".comments-form");
+form.addEventListener("submit", handleSubmit);
 
-style the form section
+function handleSubmit(e) {
+  e.preventDefault();
+  const name = e.target.userName.value;
+  const comment = e.target.userComment.value;
 
-use DOM to add dynamic html 
+  const newComment = {
+    name: name,
+    comment: comment,
+  };
 
-create an array of 3 default comments
+axios
+  .post(usersURL, newComment)
+  .then((response) => {
+      console.log(response.data);
 
-Comments must have a name, a timestamp, and the comment text
+axios
+  .get(usersURL)
+  .then((response) => {
+    commentList.innerHTML = ''
+    console.log(response.data);
 
-Make function called displayComment() that takes in one comment 
-object as a parameter and displays it on the page using JavaScript DOM manipulation
+    response.data.reverse().forEach(displayComment);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
 
-*/
+// we have posted!!
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+
+ 
+}
